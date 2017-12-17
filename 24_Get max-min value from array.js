4bdd5d65dfdfb247f8cb4b880130f4c4b79fa301
Constantin Wang <850901938@qq.com>
2017-12-17 23:45:24 +0800

    add 24

---
+++ 24_Get max-min value from array.js
@@ -0,0 +1,9 @@
+// Use Math.max() combined with the spread operator (...) to get the maximum value in the array.
+// Use Math.min() combined with the spread operator (...) to get the minimum value in the array.
+
+
+const arrayMax = arr => Math.max(...arr);
+// arrayMax([10, 1, 5]) -> 10
+
+const arrayMin = arr => Math.min(...arr);
+// arrayMin([10, 1, 5]) -> 1
